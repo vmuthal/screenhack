ba84f877cf2ac4b4688e59a5d0f85f97fe5feba2
Vivek <vivek@dreamwares.com>
2016-08-05 17:42:53 +0530

    init

---
+++ ScreenHack/Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace ScreenHack
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pbImage = new System.Windows.Forms.PictureBox();
+            this.modifyImageTimer = new System.Windows.Forms.Timer(this.components);
+            this.labelInfo = new System.Windows.Forms.Label();
+            this.labelAuthor = new System.Windows.Forms.Label();
+            this.labelTitle = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pbImage)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pbImage
+            // 
+            this.pbImage.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pbImage.Location = new System.Drawing.Point(0, 0);
+            this.pbImage.Name = "pbImage";
+            this.pbImage.Size = new System.Drawing.Size(398, 267);
+            this.pbImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbImage.TabIndex = 0;
+            this.pbImage.TabStop = false;
+            this.pbImage.Click += new System.EventHandler(this.pbImage_Click);
+            // 
+            // modifyImageTimer
+            // 
+            this.modifyImageTimer.Interval = 3000;
+            this.modifyImageTimer.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // labelInfo
+            // 
+            this.labelInfo.AutoSize = true;
+            this.labelInfo.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelInfo.Location = new System.Drawing.Point(12, 66);
+            this.labelInfo.Name = "labelInfo";
+            this.labelInfo.Size = new System.Drawing.Size(385, 150);
+            this.labelInfo.TabIndex = 1;
+            this.labelInfo.Text = "Keys - Action\r\nF10 - Start\r\nF11 - Stop\r\n\r\nDon\'t Click. Let your friend click. Enj" +
+    "oy.\r\n\r\n";
+            // 
+            // labelAuthor
+            // 
+            this.labelAuthor.AutoSize = true;
+            this.labelAuthor.Location = new System.Drawing.Point(235, 216);
+            this.labelAuthor.Name = "labelAuthor";
+            this.labelAuthor.Size = new System.Drawing.Size(151, 26);
+            this.labelAuthor.TabIndex = 2;
+            this.labelAuthor.Text = "Vivek Muthal\r\nEmail : vmuthal.18@gmail.com";
+            // 
+            // labelTitle
+            // 
+            this.labelTitle.AutoSize = true;
+            this.labelTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelTitle.Location = new System.Drawing.Point(87, 9);
+            this.labelTitle.Name = "labelTitle";
+            this.labelTitle.Size = new System.Drawing.Size(199, 37);
+            this.labelTitle.TabIndex = 3;
+            this.labelTitle.Text = "Screen Hack";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Info;
+            this.ClientSize = new System.Drawing.Size(398, 267);
+            this.Controls.Add(this.labelTitle);
+            this.Controls.Add(this.labelAuthor);
+            this.Controls.Add(this.labelInfo);
+            this.Controls.Add(this.pbImage);
+            this.Name = "MainForm";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "HiddenForm";
+            this.TopMost = true;
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.pbImage)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pbImage;
+        private System.Windows.Forms.Timer modifyImageTimer;
+        private System.Windows.Forms.Label labelInfo;
+        private System.Windows.Forms.Label labelAuthor;
+        private System.Windows.Forms.Label labelTitle;
+    }
+}
+
---
+++ ScreenHack/Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ScreenHack
+{
+    public partial class MainForm : Form
+    {
+        Bitmap bmp;
+        Boolean isStarted = false;
+        
+        public MainForm()
+        {
+            InitializeComponent();
+            this.FormBorderStyle = FormBorderStyle.None;
+            this.ShowInTaskbar = false;
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.F11)
+            {
+                modifyImageTimer.Enabled = false;
+                Application.Exit();
+            }
+            if (e.KeyCode == Keys.F10)
+            {
+                this.ShowInTaskbar = true;
+                this.WindowState = FormWindowState.Minimized;
+
+                //Create a new bitmap.
+                var bmpScreenshot = new Bitmap(Screen.PrimaryScreen.Bounds.Width,
+                                               Screen.PrimaryScreen.Bounds.Height,
+                                               PixelFormat.Format32bppArgb);
+
+                // Create a graphics object from the bitmap.
+                var gfxScreenshot = Graphics.FromImage(bmpScreenshot);
+
+                // Take the screenshot from the upper left corner to the right bottom corner.
+                gfxScreenshot.CopyFromScreen(Screen.PrimaryScreen.Bounds.X,
+                                            Screen.PrimaryScreen.Bounds.Y,
+                                            0,
+                                            0,
+                                            Screen.PrimaryScreen.Bounds.Size,
+                                            CopyPixelOperation.SourceCopy);
+
+                // Save the screenshot to the specified path that the user has chosen.
+                bmpScreenshot.Save("Screenshot.png", ImageFormat.Png);
+
+                bmp = (Bitmap)Bitmap.FromFile("Screenshot.png");
+                pbImage.Image = bmp;
+
+                labelAuthor.Visible = false;
+                labelInfo.Visible = false;
+                labelTitle.Visible = false;
+
+                isStarted = true;
+
+                this.ShowInTaskbar = false;
+                this.WindowState = FormWindowState.Maximized;
+
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+
+            for (int x = rnd.Next(0, bmp.Width); x < bmp.Width; x++)
+            {
+                for (int y = rnd.Next(0, bmp.Height); y < bmp.Height; y++)
+                {
+                    if (rnd.Next(0, 100) % 2 == 0)
+                        bmp.SetPixel(x, y, Color.Black);                    
+                }
+            }
+
+            pbImage.Image = bmp;
+            
+        }
+
+        private void pbImage_Click(object sender, EventArgs e)
+        {
+            if (!modifyImageTimer.Enabled && isStarted)
+            {
+                isStarted = true;
+                modifyImageTimer.Enabled = true;
+            }
+        }
+
+    }
+}
